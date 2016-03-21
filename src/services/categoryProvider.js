af167d018ef68040d6cdfb49c0368a6e794d8a18
DriArez <arezkidrir@htomail.com>
2016-03-21 09:07:45 +0100

    fix bug

--- src/services/categoryProvider.js
+++ src/services/categoryProvider.js
@@ -1,12 +1,17 @@
 'use strict';
 
+var categories = [
+        {'id': 1, 'name': 'films'},
+        {'id': 2, 'name': 'musiques'}
+    ];
+
 app.service('categoryProvider', function ($firebaseArray) {
 
     var ref =  new Firebase("https://popping-inferno-6997.firebaseio.com/");
 
     var sync = $firebaseArray(ref);
     //ref.push(categories);
-    //sync.$add({"categories" : categories});
+    sync.$add(categories);
     this.getCategories = function () {
         return categories;
     };
--- src/services/itemProvider.js
+++ src/services/itemProvider.js
@@ -1,12 +1,16 @@
 'use strict';
 
+var items = [
+        {'name': 'Django Unchained', 'category_id': 1},
+        {'name': 'Forrest Gump', 'category_id': 1}
+    ];
 app.service('itemProvider', function ($firebaseArray) {
 
     var ref =  new Firebase("https://popping-inferno-6997.firebaseio.com/");
-
+    // ref.push(items);
     var sync = $firebaseArray(ref);
-
-    //sync.$add('items' : items);
+    
+    sync.$add(items);
 
     this.getItems = function () {
         return items;
